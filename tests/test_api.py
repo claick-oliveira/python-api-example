4021dbb7e4e1523cde791a0e2faa6524953895e9
claick-oliveira <claick.cao@gmail.com>
2019-12-19 16:28:10 -0300

    Added docstrings in test files and api files

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,15 @@ import json
 
 class TestTransactions():
     def test_index(self):
+        '''
+        This function does a GET on the path / and
+        validate if the response body has data and the status code.
+
+        Test:
+            data['msg'] == 'Python API example!'
+            data['status'] == 200
+            response.status_code == 200
+        '''
         response = app.test_client().get(
             '/',
             content_type='application/json'
@@ -16,6 +25,14 @@ class TestTransactions():
         assert data['status'] == 200
 
     def test_get(self):
+        '''
+        This function does a GET on the path /api/unicorns and
+        validate if the response body has data and the status code.
+
+        Test:
+            len(data['unicorns']) == 10
+            response.status_code == 200
+        '''
         response = app.test_client().get(
             '/api/unicorns',
             content_type='application/json'
@@ -27,6 +44,19 @@ class TestTransactions():
         assert len(data['unicorns']) == 10
 
     def test_get_id(self):
+        '''
+        This function does a GET on the path /api/unicorns/1 and
+        validate if the response body has data and the status code.
+
+        Test:
+            response.status_code == 200
+            len(data['unicorn']) == 5
+            data['unicorn']['id'] == 1
+            data['unicorn']['name'] != ''
+            data['unicorn']['location'] != ''
+            data['unicorn']['info'] != ''
+            data['unicorn']['description'] != ''
+        '''
         response = app.test_client().get(
             '/api/unicorns/1',
             content_type='application/json'
@@ -43,6 +73,14 @@ class TestTransactions():
         assert data['unicorn']['description'] != ''
 
     def test_get_id_404(self):
+        '''
+        This function does a GET on the path /api/unicorns/404 and
+        validate if the response body has data and the status code.
+
+        Test:
+            response.status_code == 404
+            data['error'] == 'Not found'
+        '''
         response = app.test_client().get(
             '/api/unicorns/404',
             content_type='application/json'
@@ -54,6 +92,29 @@ class TestTransactions():
         assert data['error'] == 'Not found'
 
     def test_post(self):
+        '''
+        This function does a POST on the path /api/unicorns to add a
+        new unicorn, a GET on the path /api/unicorns/<id> and validate
+        if the data was added.
+
+        Test:
+            POST:
+            response_post.status_code == 201
+            len(data_post['unicorn']) == 5
+            data_post['unicorn']['name'] == 'Test Unicorn'
+            data_post['unicorn']['location'] == 'Test'
+            data_post['unicorn']['info'] == ''
+            data_post['unicorn']['description'] == 'Test unicorn was added!'
+
+            GET:
+            response_get.status_code == 200
+            len(data_post['unicorn']) == 5
+            data_get['unicorn']['id'] == data_post['unicorn']['id']
+            data_get['unicorn']['name'] == 'Test Unicorn'
+            data_get['unicorn']['location'] == 'Test'
+            data_get['unicorn']['info'] == ''
+            data_get['unicorn']['description'] == 'Test unicorn was added!'
+        '''
         response_post = app.test_client().post(
             '/api/unicorns',
             data=json.dumps({
@@ -87,6 +148,30 @@ class TestTransactions():
         assert data_get['unicorn']['description'] == 'Test unicorn was added!'
 
     def test_put(self):
+        '''
+        This function does a PUT on the path /api/unicorns to update a
+        some unicorn, a GET on the path /api/unicorns/<id> and validate
+        if the data was updated.
+
+        Test:
+
+            PUT:
+            response_post.status_code == 201
+            len(data_post['unicorn']) == 5
+            data_post['unicorn']['name'] == 'Test Unicorn'
+            data_post['unicorn']['location'] == 'Test'
+            data_post['unicorn']['info'] == ''
+            data_post['unicorn']['description'] == 'Test unicorn was added!'
+
+            GET:
+            response_put.status_code == 200
+            len(data_post['unicorn']) == 5
+            data_put['unicorn']['id'] == data_post['unicorn']['id']
+            data_put['unicorn']['name'] == 'Put Unicorn'
+            data_put['unicorn']['location'] == 'Put'
+            data_put['unicorn']['info'] == ''
+            data_put['unicorn']['description'] == 'Put unicorn was added!'
+        '''
         response_post = app.test_client().post(
             '/api/unicorns',
             data=json.dumps({
@@ -127,6 +212,25 @@ class TestTransactions():
         assert data_put['unicorn']['description'] == 'Put unicorn was added!'
 
     def test_delete(self):
+        '''
+        This function does a POST on the path /api/unicorns to add a
+        new unicorn, a DELETE on the path /api/unicorns/<id> and validate
+        if the data was deleted.
+
+        Test:
+
+            POST:
+            response_post.status_code == 201
+            len(data_post['unicorn']) == 5
+            data_post['unicorn']['name'] == 'Test Unicorn'
+            data_post['unicorn']['location'] == 'Test'
+            data_post['unicorn']['info'] == ''
+            data_post['unicorn']['description'] == 'Test unicorn was added!'
+
+            DELETE:
+            response_put.status_code == 200
+            data_put['result'] == True
+        '''
         response_post = app.test_client().post(
             '/api/unicorns',
             data=json.dumps({
